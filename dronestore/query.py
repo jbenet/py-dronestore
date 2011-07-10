e1e201c5e0b6206bb648ae9fb82650cb57aecfc4
Juan Batiz-Benet <jbenet@cs.stanford.edu>
2011-07-10 03:24:21 -0700

    Added Query objects

---
+++ dronestore/query.py
@@ -0,0 +1,241 @@
+
+from model import Key, Version, Model
+
+
+class Filter(object):
+  '''Represents a Filter for a specific field and its value.
+  Filters are used on queries to narrow down the set of matching objects.
+  '''
+
+  CONDITIONAL_OPERATORS = {
+    "<"  : lambda a, b: a < b,
+    "<=" : lambda a, b: a <= b,
+    "="  : lambda a, b: a == b,
+    "!=" : lambda a, b: a != b,
+    ">=" : lambda a, b: a >= b,
+    ">"  : lambda a, b: a > b
+  }
+
+  def __init__(self, field, op, value):
+    if op not in self.CONDITIONAL_OPERATORS:
+      raise ValueError('"%s" is not a valid filter Conditional Operator' % op)
+
+    self.field = field
+    self.op = op
+    self.value = value
+
+  def __call__(self, version):
+    '''Returns whether this version passes this filter.'''
+    serialRep = version.serialRepresentation()
+
+    value = None
+    if self.field in serialRep:
+      value = serialRep[self.field]
+    elif self.field in serialRep['attributes']:
+      value = serialRep['attributes'][self.field]['value']
+    else:
+      return self.value is None # can't find it. were we meant not to?
+
+    if isinstance(self.value, str) and not isinstance(value, str):
+      value = str(value)
+    return self.valuePasses(value)
+
+  def valuePasses(self, value):
+    '''Returns whether this value passes this filter'''
+    return self.CONDITIONAL_OPERATORS[self.op](value, self.value)
+
+
+  def __str__(self):
+    return '%s %s %s' % (self.field, self.op, self.value)
+
+  def __repr__(self):
+    return "Filter('%s', '%s', %s)" % (self.field, self.op, repr(self.value))
+
+
+  def __eq__(self, o):
+    return self.field == o.field and self.op == o.op and self.value == o.value
+
+  def __ne__(self, other):
+    return not self.__eq__(other)
+
+  def __hash__(self):
+    return hash(repr(self))
+
+
+
+
+class Order(object):
+  '''Represents an Order upon a specific field, and a direction.
+  Orders are used on queries to define how they operate on objects
+  '''
+
+  ORDER_OPERATORS = ['-', '+']
+
+  def __init__(self, order):
+    self.op = '+'
+
+    try:
+      if order[0] in self.ORDER_OPERATORS:
+        self.op = order[0]
+        order = order[1:]
+    except IndexError:
+      raise ValueError('Order input be at least two characters long.')
+
+    self.field = order
+
+    if self.op not in self.ORDER_OPERATORS:
+      raise ValueError('"%s" is not a valid Order Operator.' % op)
+
+
+  def __str__(self):
+    return '%s%s' % (self.op, self.field)
+
+  def __repr__(self):
+    return "Order('%s%s')" % (self.op, self.field)
+
+
+  def __eq__(self, other):
+    return self.field == other.field and self.op == other.op
+
+  def __ne__(self, other):
+    return not self.__eq__(other)
+
+  def __hash__(self):
+    return hash(repr(self))
+
+
+  def isAscending(self):
+    return self.op == '+'
+
+  def isDescending(self):
+    return not self.isAscending()
+
+
+  def keyfn(self, version):
+    '''A key function to be used in pythonic sort operations.'''
+    serialRep = version.serialRepresentation()
+
+    if self.field in serialRep:
+      value = serialRep[self.field]
+    elif self.field in serialRep['attributes']:
+      value = serialRep['attributes'][self.field]['value']
+    else:
+      value = self.value is None # can't find it. were we meant not to?
+
+    return value
+
+  @staticmethod
+  def sorted(items, orders):
+    comparers = [ (o.keyfn, 1 if o.isAscending() else -1) for o in orders]
+
+    def cmpfn(a, b):
+      for keyfn, ascOrDesc in comparers:
+        comparison = cmp(keyfn(a), keyfn(b)) * ascOrDesc
+        if comparison is not 0:
+          return comparison
+      return 0
+
+    return sorted(items, cmp=cmpfn)
+
+
+
+
+
+class Query(object):
+  '''Represents a version Query, used to describe a set versions. Queries are
+  used to retrieve versions and instances matching a set of criteria from
+  Datastores and Drones. Query objects themselves are simply descriptions,
+  the actual implementations are left up to the Datastores.
+  '''
+
+  DEFAULT_LIMIT = 2000
+
+  def __init__(self, dstype, limit=None, offset=0, keysonly=False):
+    self.type = dstype
+    self.model = Model.modelNamed(dstype)
+
+    self.limit = limit if limit is not None else self.DEFAULT_LIMIT
+    self.offset = offset
+    self.keysonly = keysonly
+
+    self._filters = []
+    self._orders = []
+
+  def __str__(self):
+    return '<%s for %s>' % (self.__class__, self.model.__class__)
+
+  def __repr__(self):
+    return 'Query.from_dict(%s)' % self.dict()
+
+  def order(self, order):
+    '''Adds an Order to this query.
+
+    Returns self for JS-like method chaining:
+    query.filter('age', '>', 18).filter('sex', '=', 'Female')
+    '''
+
+    order = order if isinstance(order, Order) else Order(order)
+    self._orders.append(order)
+    return self
+
+
+  def filter(self, *args):
+    '''Adds a Filter to this query.
+
+    Returns self for JS-like method chaining:
+    query.filter('age', '>', 18).filter('sex', '=', 'Female')
+    '''
+    if len(args) == 1 and isinstance(args[0], Filter):
+      filter = args[0]
+    else:
+      filter = Filter(*args)
+    self._filters.append(filter)
+    return self # for JSlike chaining: q.filter('age', '>', 18).filter(...)
+
+
+  def __cmp__(self, other):
+    return cmp(self.dict(), other.dict())
+
+  def __hash__(self):
+    return hash(repr(self))
+
+
+  def dict(self):
+    '''Returns a dictionary representing this query.'''
+
+    d = dict( { 'type' : self.type } )
+
+    if self.limit != self.DEFAULT_LIMIT:
+      d['limit'] = self.limit
+    if self.offset > 0:
+      d['offset'] = self.offset
+    if len(self._filters) > 0:
+      d['filter'] = self._filters
+    if len(self._orders) > 0:
+      d['order'] = self._orders
+    if self.keysonly:
+      d['keysonly'] = self.keysonly
+
+    return d
+
+  @classmethod
+  def from_dict(cls, dictionary):
+    '''Constructs a query from a dictionary.'''
+    query = cls(dictionary['type'])
+    for key, value in dictionary.items():
+
+      if key == 'order':
+        for order in value:
+          query.order(order)
+
+      elif key == 'filter':
+        for filter in value:
+          if not isinstance(filter, Filter):
+            filter = Filter(*filter)
+          query.filter(filter)
+
+      elif key in ['limit', 'offset', 'keysonly']:
+        setattr(query, key, value)
+    return query
+
+
---
+++ test/test_query.py
@@ -0,0 +1,353 @@
+import time
+import datetime
+import unittest
+import hashlib
+import nanotime
+
+from dronestore.model import Key, Version, Model
+from dronestore.query import Filter, Order, Query
+from dronestore.util import serial
+from dronestore import model
+
+
+def versions():
+  sr = serial.SerialRepresentation()
+  sr['key'] = '/ABCD'
+  sr['hash'] = hashlib.sha1('herp').hexdigest()
+  sr['parent'] = Version.BLANK_HASH
+  sr['created'] = nanotime.now().nanoseconds()
+  sr['committed'] = nanotime.now().nanoseconds()
+  sr['attributes'] = {'str' : {'value' : 'herp'} }
+  sr['type'] = 'Hurr'
+
+  v1 = Version(sr)
+
+  sr = serial.SerialRepresentation()
+  sr['key'] = '/ABCD'
+  sr['hash'] = hashlib.sha1('derp').hexdigest()
+  sr['parent'] = hashlib.sha1('herp').hexdigest()
+  sr['created'] = nanotime.now().nanoseconds()
+  sr['committed'] = nanotime.now().nanoseconds()
+  sr['attributes'] = {'str' : {'value' : 'derp'} }
+  sr['type'] = 'Hurr'
+
+  v2 = Version(sr)
+
+  sr = serial.SerialRepresentation()
+  sr['key'] = '/ABCD'
+  sr['hash'] = hashlib.sha1('lerp').hexdigest()
+  sr['parent'] = hashlib.sha1('derp').hexdigest()
+  sr['created'] = nanotime.now().nanoseconds()
+  sr['committed'] = nanotime.now().nanoseconds()
+  sr['attributes'] = {'str' : {'value' : 'lerp'} }
+  sr['type'] = 'Hurr'
+
+  v3 = Version(sr)
+
+  return v1, v2, v3
+
+
+class TestFilter(unittest.TestCase):
+
+  def test_basic(self):
+
+    v1, v2, v3 = versions()
+
+    t1 = v1.committed()
+    t2 = v2.committed()
+    t3 = v3.committed()
+
+    fkgtA = Filter('key', '>', '/A')
+
+    self.assertTrue(fkgtA(v1))
+    self.assertTrue(fkgtA(v2))
+    self.assertTrue(fkgtA(v3))
+
+    self.assertTrue(fkgtA.valuePasses('/BCDEG'))
+    self.assertTrue(fkgtA.valuePasses('/ZCDEFDSA/fdsafdsa/fdsafdsaf'))
+    self.assertFalse(fkgtA.valuePasses('/6353456346543'))
+    self.assertFalse(fkgtA.valuePasses('.'))
+    self.assertTrue(fkgtA.valuePasses('afsdafdsa'))
+
+    fkltA = Filter('key', '<', '/A')
+
+    self.assertFalse(fkltA(v1))
+    self.assertFalse(fkltA(v2))
+    self.assertFalse(fkltA(v3))
+
+    self.assertFalse(fkltA.valuePasses('/BCDEG'))
+    self.assertFalse(fkltA.valuePasses('/ZCDEFDSA/fdsafdsa/fdsafdsaf'))
+    self.assertTrue(fkltA.valuePasses('/6353456346543'))
+    self.assertFalse(fkgtA.valuePasses('.'))
+    self.assertFalse(fkltA.valuePasses('A'))
+    self.assertFalse(fkltA.valuePasses('afsdafdsa'))
+
+    fkeqA = Filter('key', '=', '/ABCD')
+
+    self.assertTrue(fkeqA(v1))
+    self.assertTrue(fkeqA(v2))
+    self.assertTrue(fkeqA(v3))
+
+    self.assertFalse(fkeqA.valuePasses('/BCDEG'))
+    self.assertFalse(fkeqA.valuePasses('/ZCDEFDSA/fdsafdsa/fdsafdsaf'))
+    self.assertFalse(fkeqA.valuePasses('/6353456346543'))
+    self.assertFalse(fkeqA.valuePasses('A'))
+    self.assertFalse(fkeqA.valuePasses('.'))
+    self.assertFalse(fkeqA.valuePasses('afsdafdsa'))
+    self.assertTrue(fkeqA.valuePasses('/ABCD'))
+
+
+    fkgtB = Filter('key', '>', '/B')
+
+    self.assertFalse(fkgtB(v1))
+    self.assertFalse(fkgtB(v2))
+    self.assertFalse(fkgtB(v3))
+
+    self.assertFalse(fkgtB.valuePasses('/A'))
+    self.assertTrue(fkgtB.valuePasses('/BCDEG'))
+    self.assertTrue(fkgtB.valuePasses('/ZCDEFDSA/fdsafdsa/fdsafdsaf'))
+    self.assertFalse(fkgtB.valuePasses('/6353456346543'))
+    self.assertFalse(fkgtB.valuePasses('.'))
+    self.assertTrue(fkgtB.valuePasses('A'))
+    self.assertTrue(fkgtB.valuePasses('afsdafdsa'))
+
+    fkltB = Filter('key', '<', '/B')
+
+    self.assertTrue(fkltB(v1))
+    self.assertTrue(fkltB(v2))
+    self.assertTrue(fkltB(v3))
+
+    self.assertTrue(fkltB.valuePasses('/A'))
+    self.assertFalse(fkltB.valuePasses('/BCDEG'))
+    self.assertFalse(fkltB.valuePasses('/ZCDEFDSA/fdsafdsa/fdsafdsaf'))
+    self.assertTrue(fkltB.valuePasses('/6353456346543'))
+    self.assertTrue(fkltB.valuePasses('.'))
+    self.assertFalse(fkltB.valuePasses('A'))
+    self.assertFalse(fkltB.valuePasses('afsdafdsa'))
+
+    fkgtAB = Filter('key', '>', '/AB')
+
+
+    self.assertTrue(fkgtAB(v1))
+    self.assertTrue(fkgtAB(v2))
+    self.assertTrue(fkgtAB(v3))
+
+    self.assertFalse(fkgtAB.valuePasses('/A'))
+    self.assertTrue(fkgtAB.valuePasses('/BCDEG'))
+    self.assertTrue(fkgtAB.valuePasses('/ZCDEFDSA/fdsafdsa/fdsafdsaf'))
+    self.assertFalse(fkgtAB.valuePasses('/6353456346543'))
+    self.assertFalse(fkgtAB.valuePasses('.'))
+    self.assertTrue(fkgtAB.valuePasses('A'))
+    self.assertTrue(fkgtAB.valuePasses('afsdafdsa'))
+
+    fgtet1 = Filter('committed', '>=', t1)
+    fgtet2 = Filter('committed', '>=', t2)
+    fgtet3 = Filter('committed', '>=', t3)
+
+    self.assertTrue(fgtet1(v1))
+    self.assertTrue(fgtet1(v2))
+    self.assertTrue(fgtet1(v3))
+
+    self.assertFalse(fgtet2(v1))
+    self.assertTrue(fgtet2(v2))
+    self.assertTrue(fgtet2(v3))
+
+    self.assertFalse(fgtet3(v1))
+    self.assertFalse(fgtet3(v2))
+    self.assertTrue(fgtet3(v3))
+
+    fltet1 = Filter('committed', '<=', t1)
+    fltet2 = Filter('committed', '<=', t2)
+    fltet3 = Filter('committed', '<=', t3)
+
+    self.assertTrue(fltet1(v1))
+    self.assertFalse(fltet1(v2))
+    self.assertFalse(fltet1(v3))
+
+    self.assertTrue(fltet2(v1))
+    self.assertTrue(fltet2(v2))
+    self.assertFalse(fltet2(v3))
+
+    self.assertTrue(fltet3(v1))
+    self.assertTrue(fltet3(v2))
+    self.assertTrue(fltet3(v3))
+
+    feqt1 = Filter('committed', '=', t1)
+    feqt2 = Filter('committed', '=', t2)
+    feqt3 = Filter('committed', '=', t3)
+
+    self.assertTrue(feqt1(v1))
+    self.assertFalse(feqt1(v2))
+    self.assertFalse(feqt1(v3))
+
+    self.assertFalse(feqt2(v1))
+    self.assertTrue(feqt2(v2))
+    self.assertFalse(feqt2(v3))
+
+    self.assertFalse(feqt3(v1))
+    self.assertFalse(feqt3(v2))
+    self.assertTrue(feqt3(v3))
+
+
+  def test_object(self):
+    t1 = nanotime.now()
+    t2 = nanotime.now()
+
+    f1 = Filter('key', '>', '/A')
+    f2 = Filter('key', '<', '/A')
+    f3 = Filter('committed', '=', t1)
+    f4 = Filter('committed', '>=', t2)
+
+    self.assertEqual(f1, eval(repr(f1)))
+    self.assertEqual(f2, eval(repr(f2)))
+    self.assertEqual(f3, eval(repr(f3)))
+    self.assertEqual(f4, eval(repr(f4)))
+
+    self.assertEqual(str(f1), 'key > /A')
+    self.assertEqual(str(f2), 'key < /A')
+    self.assertEqual(str(f3), 'committed = %s' % t1)
+    self.assertEqual(str(f4), 'committed >= %s' % t2)
+
+    self.assertEqual(f1, Filter('key', '>', '/A'))
+    self.assertEqual(f2, Filter('key', '<', '/A'))
+    self.assertEqual(f3, Filter('committed', '=', t1))
+    self.assertEqual(f4, Filter('committed', '>=', t2))
+
+    self.assertNotEqual(f2, Filter('key', '>', '/A'))
+    self.assertNotEqual(f1, Filter('key', '<', '/A'))
+    self.assertNotEqual(f4, Filter('committed', '=', t1))
+    self.assertNotEqual(f3, Filter('committed', '>=', t2))
+
+    self.assertEqual(hash(f1), hash(Filter('key', '>', '/A')))
+    self.assertEqual(hash(f2), hash(Filter('key', '<', '/A')))
+    self.assertEqual(hash(f3), hash(Filter('committed', '=', t1)))
+    self.assertEqual(hash(f4), hash(Filter('committed', '>=', t2)))
+
+    self.assertNotEqual(hash(f2), hash(Filter('key', '>', '/A')))
+    self.assertNotEqual(hash(f1), hash(Filter('key', '<', '/A')))
+    self.assertNotEqual(hash(f4), hash(Filter('committed', '=', t1)))
+    self.assertNotEqual(hash(f3), hash(Filter('committed', '>=', t2)))
+
+
+
+class TestOrder(unittest.TestCase):
+
+  def test_basic(self):
+    o1 = Order('key')
+    o2 = Order('+committed')
+    o3 = Order('-created')
+
+    v1, v2, v3 = versions()
+
+    # test  isAscending
+    self.assertTrue(o1.isAscending())
+    self.assertTrue(o2.isAscending())
+    self.assertFalse(o3.isAscending())
+
+    # test keyfn
+    self.assertEqual(o1.keyfn(v1), str(v1.key()))
+    self.assertEqual(o1.keyfn(v2), str(v2.key()))
+    self.assertEqual(o1.keyfn(v3), str(v3.key()))
+    self.assertEqual(o1.keyfn(v1), str(v2.key()))
+    self.assertEqual(o1.keyfn(v1), str(v3.key()))
+
+    self.assertEqual(o2.keyfn(v1), int(v1.committed()))
+    self.assertEqual(o2.keyfn(v2), int(v2.committed()))
+    self.assertEqual(o2.keyfn(v3), int(v3.committed()))
+    self.assertNotEqual(o2.keyfn(v1), int(v2.committed()))
+    self.assertNotEqual(o2.keyfn(v1), int(v3.committed()))
+
+    self.assertEqual(o3.keyfn(v1), int(v1.created()))
+    self.assertEqual(o3.keyfn(v2), int(v2.created()))
+    self.assertEqual(o3.keyfn(v3), int(v3.created()))
+    self.assertNotEqual(o3.keyfn(v1), int(v2.created()))
+    self.assertNotEqual(o3.keyfn(v1), int(v3.created()))
+
+    # test sorted
+    self.assertEqual(Order.sorted([v3, v2, v1], [o1]), [v3, v2, v1])
+    self.assertEqual(Order.sorted([v3, v2, v1], [o1, o2]), [v1, v2, v3])
+    self.assertEqual(Order.sorted([v1, v3, v2], [o1, o3]), [v3, v2, v1])
+    self.assertEqual(Order.sorted([v3, v2, v1], [o1, o2, o3]), [v1, v2, v3])
+    self.assertEqual(Order.sorted([v1, v3, v2], [o1, o3, o2]), [v3, v2, v1])
+
+    self.assertEqual(Order.sorted([v3, v2, v1], [o2]), [v1, v2, v3])
+    self.assertEqual(Order.sorted([v3, v2, v1], [o2, o1]), [v1, v2, v3])
+    self.assertEqual(Order.sorted([v3, v2, v1], [o2, o3]), [v1, v2, v3])
+    self.assertEqual(Order.sorted([v3, v2, v1], [o2, o1, o3]), [v1, v2, v3])
+    self.assertEqual(Order.sorted([v3, v2, v1], [o2, o3, o1]), [v1, v2, v3])
+
+    self.assertEqual(Order.sorted([v1, v2, v3], [o3]), [v3, v2, v1])
+    self.assertEqual(Order.sorted([v1, v2, v3], [o3, o2]), [v3, v2, v1])
+    self.assertEqual(Order.sorted([v1, v2, v3], [o3, o1]), [v3, v2, v1])
+    self.assertEqual(Order.sorted([v1, v2, v3], [o3, o2, o1]), [v3, v2, v1])
+    self.assertEqual(Order.sorted([v1, v2, v3], [o3, o1, o2]), [v3, v2, v1])
+
+
+  def test_object(self):
+    self.assertEqual(Order('key'), eval(repr(Order('key'))))
+    self.assertEqual(Order('+committed'), eval(repr(Order('+committed'))))
+    self.assertEqual(Order('-created'), eval(repr(Order('-created'))))
+
+    self.assertEqual(str(Order('key')), '+key')
+    self.assertEqual(str(Order('+committed')), '+committed')
+    self.assertEqual(str(Order('-created')), '-created')
+
+    self.assertEqual(Order('key'), Order('+key'))
+    self.assertEqual(Order('-key'), Order('-key'))
+    self.assertEqual(Order('+committed'), Order('+committed'))
+
+    self.assertNotEqual(Order('key'), Order('-key'))
+    self.assertNotEqual(Order('+key'), Order('-key'))
+    self.assertNotEqual(Order('+committed'), Order('+key'))
+
+    self.assertEqual(hash(Order('+key')), hash(Order('+key')))
+    self.assertEqual(hash(Order('-key')), hash(Order('-key')))
+    self.assertNotEqual(hash(Order('+key')), hash(Order('-key')))
+    self.assertEqual(hash(Order('+committed')), hash(Order('+committed')))
+    self.assertNotEqual(hash(Order('+committed')), hash(Order('+key')))
+
+
+class TestQuery(unittest.TestCase):
+
+  def test_basic(self):
+    fn = lambda: Query('ModelNotRegistered')
+    self.assertRaises(model.UnregisteredModelError, fn)
+    now = nanotime.now().nanoseconds()
+
+    q1 = Query('Model', limit=100)
+    q2 = Query('Model', offset=200)
+    q3 = Query('Model', keysonly=True)
+
+    q1.offset = 300
+    q2.keysonly = True
+    q3.limit = 1
+
+    q1.filter('key', '>', '/ABC')
+    q1.filter('created', '>', now)
+
+    q2.order('key')
+    q2.order('-created')
+
+    q1d = {'type' : 'Model', 'limit':100, 'offset':300, \
+      'filter': [Filter('key', '>', '/ABC'), Filter('created', '>', now)] }
+
+    q2d = {'type' : 'Model', 'offset':200, 'keysonly':True,
+      'order': [ Order('+key'), Order('-created') ] }
+
+    q3d = {'type' : 'Model', 'limit':1, 'keysonly':True}
+
+    self.assertEqual(q1.dict(), q1d)
+    self.assertEqual(q2.dict(), q2d)
+    self.assertEqual(q3.dict(), q3d)
+
+    self.assertEqual(q1, Query.from_dict(q1d))
+    self.assertEqual(q2, Query.from_dict(q2d))
+    self.assertEqual(q3, Query.from_dict(q3d))
+
+    self.assertEqual(q1, eval(repr(q1)))
+    self.assertEqual(q2, eval(repr(q2)))
+    self.assertEqual(q3, eval(repr(q3)))
+
+
+if __name__ == '__main__':
+  unittest.main()
