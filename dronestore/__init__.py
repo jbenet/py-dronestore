a48004af1c0b6d9e5fc55374083f846449b8ad68
Juan Batiz-Benet <jbenet@cs.stanford.edu>
2011-07-12 01:24:06 -0700

    Added top level imports

--- dronestore/__init__.py
+++ dronestore/__init__.py
@@ -1,7 +1,26 @@
 
 __version__ = '0.1.5'
+__author__ = 'Juan Batiz-Benet <jbenet@cs.stanford.edu>'
 
-from model import Key, Version, Model
+
+# basic data model
+from model import Key
+from model import Version
+from model import Model
+
+# attributes
+from model import Attribute
+from model import StringAttribute
+from model import KeyAttribute
+from model import IntegerAttribute
+from model import TimeAttribute
+
+# drones
 from drone import Drone
-from datastore import Datastore, DictDatastore
 from query import Query
+
+# basic datastores
+from datastore import Datastore
+from datastore import DictDatastore
+
+
